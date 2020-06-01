93a2b42ce1bf070041bc29bce59bcd69fb18edfc
mepiscope <mepiscope@globalblue.com>
2020-06-01 13:47:03 +0800

    Added IE11 support. Added favicon.

--- master.js
+++ master.js
@@ -1,5 +1,6 @@
 function changeSigninText() {
-    var elements = window.document.querySelectorAll('div.intro h2, div.buttons button#next');
+    // Added slice to support IE 11
+    var elements = Array.prototype.slice.call(window.document.querySelectorAll('div.intro h2, div.buttons button#next'), 0);    
 
     elements.forEach(function(e) {
         e.textContent = 'Log in';
@@ -11,7 +12,7 @@ function setupSpinner() {
 
     if (!divButtons) return;
 
-    var buttons = divButtons.querySelectorAll('button#next, button#verifyCode, button#cancel');
+    var buttons = Array.prototype.slice.call(divButtons.querySelectorAll('button#next, button#verifyCode, button#cancel'), 0);
     var spinner = window.document.querySelector('div.spinner-overlay');
 
     spinner.style.display = 'none';
@@ -24,12 +25,12 @@ function setupSpinner() {
     });
 
     //Observe the paragraph
-    var pObserver = new window.MutationObserver( function(mutations) {
+    var pObserver = new window.MutationObserver(function(mutations) {
         spinner.style.display = 'none';
     }.bind(this));
 
-    var paragraphs = window.document.querySelectorAll('div.error p');
-
+    var paragraphs = Array.prototype.slice.call(window.document.querySelectorAll('div.error p'), 0);
+    
     paragraphs.forEach(function(p) {
         pObserver.observe(p, {characterData: true, childList: true});
     });
