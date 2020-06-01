3c259dc38f9baf39762f17c988ca4867b1dc49cd
mepiscope <mepiscope@globalblue.com>
2020-06-01 12:39:00 +0800

    Used array foreach. Added 500ms delay for initializing controls.

--- master.js
+++ master.js
@@ -23,41 +23,60 @@
 //});
 
 function changeSigninText() {
-    var elements = document.querySelectorAll('div.intro h2, div.buttons button#next');
+    var elements = window.document.querySelectorAll('div.intro h2, div.buttons button#next');
 
-    for (var i = 0; i < elements.length; i++) {
-        elements[i].textContent = 'Log in';
-    }
+    elements.forEach(function(e) {
+        e.textContent = 'Log in';
+    });
+
+    //for (var i = 0; i < elements.length; i++) {
+    //    elements[i].textContent = 'Log in';
+    //}
 }
 
 function toggleSpinner() {
-    var buttons = document.querySelector('div.buttons').querySelectorAll('button#next, button#verifyCode, button#cancel');
-    var spinner = document.querySelector('div.spinner-overlay');
+    var divButtons = window.document.querySelector('div.buttons');
+
+    if (!divButtons) return;
+
+    var buttons = divButtons.querySelectorAll('button#next, button#verifyCode, button#cancel');
+    var spinner = window.document.querySelector('div.spinner-overlay');
 
     spinner.style.display = 'none';
 
-    for (i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click',
+    buttons.forEach(function(b) {
+        b.addEventListener('click',
             function() {
                 spinner.style.display = 'block';
             });
-    }
+    });
+
+    //for (i = 0; i < buttons.length; i++) {
+    //    buttons[i].addEventListener('click',
+    //        function() {
+    //            spinner.style.display = 'block';
+    //        });
+    //}
 
     //Observe the paragraph
     var pObserver = new window.MutationObserver( function(mutations) {
         spinner.style.display = 'none';
     }.bind(this));
 
-    var paragraphs = document.querySelectorAll('div.error p');
+    var paragraphs = window.document.querySelectorAll('div.error p');
 
-    for (i = 0; i < paragraphs.length; i++) {
-        pObserver.observe(paragraphs[i], {characterData: true, childList: true});
-    }
+    paragraphs.forEach(function(p) {
+        pObserver.observe(p, {characterData: true, childList: true});
+    });
+
+    //for (i = 0; i < paragraphs.length; i++) {
+    //    pObserver.observe(paragraphs[i], {characterData: true, childList: true});
+    //}
 }
 
-function initialize() {
+function initializeControls() {
     changeSigninText();
     toggleSpinner();
 }
 
-initialize();
+setTimeout(initializeControls(), 500);
