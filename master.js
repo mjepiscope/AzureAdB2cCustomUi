78323a984e7343873231ca65e5a495bd009a86f5
mepiscope <mepiscope@globalblue.com>
2020-06-01 11:28:46 +0800

    Added jQuery reference Show spinning wheel on signin and send code buttons and hide when error is displayed

--- master.js
+++ master.js
@@ -1,1 +1,23 @@
-// JavaScript source code
+$('document').ready(function() {
+
+    // Change text to Log in (MPP default)
+    $('div.intro h2, div.buttons button#next').text('Log in');
+
+    // Toggle the spinner after button click and after error is displayed
+    $('div.spinner-overlay').hide();
+
+    $('div.buttons')
+        .find('button#next, button#verifyCode, button#cancel')
+        .click(function() {
+            $('div.spinner-overlay').show();
+    });
+
+    //Observe the paragraph
+    var pObserver = new window.MutationObserver( function(mutations) {
+        $('div.spinner-overlay').hide();
+    }.bind(this));
+
+    $('div.error').find('p').each(function() {
+        pObserver.observe(this, {characterData: true, childList: true});
+    });
+});
